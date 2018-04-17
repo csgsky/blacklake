08415ff5887ec2f9dceeaf62184b594381671b31
csgsky <674143140@qq.com>
2018-04-17 12:38:56 +0800

    black lake init

---
+++ src/page/Splash.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import {View} from 'react-native';
+
+
+export default class Splash extends Component {
+  render() {
+    return (
+      <View style={{
+        width: 200,
+        height: 200,
+        backgroundColor: 'red'
+      }}/>
+    );
+  }
+}
