c1d99a3049923d9cdd55f6452935b093824017c5
csgsky <674143140@qq.com>
2018-04-18 09:39:25 +0800

    Rx + Redux

--- src/page/splash/Splash.js
+++ src/page/splash/Splash.js
@@ -1,9 +1,17 @@
 import React, {Component} from 'react';
-import {View, Image, TouchableOpacity} from 'react-native';
-import { Observable } from 'rxjs/Rx'
-import SplashImage from '../../img/launch_screen.png'
+import {View, Image} from 'react-native';
+import { Observable } from 'rxjs/Rx';
+import { NavigationActions } from 'react-navigation';
+import SplashImage from '../../img/launch_screen.png';
 
 import theme from '../../config/theme'
+
+const resetActionLogin = NavigationActions.reset({
+  index: 0,
+  actions: [
+    NavigationActions.navigate({routeName: 'Login'})
+  ]
+})
 
 export default class Splash extends Component {
 
@@ -15,6 +23,7 @@ export default class Splash extends Component {
   }
 
   componentDidMount() {
+    // eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0eXBlIjoiQWNjb3VudFR5cGUiLCJpZCI6MTYsInVpZCI6IjEzMDAwMDAwMDAwIiwiZW1haWwiOm51bGwsIm5hbWUiOiLmtYvor5XnrqHnkIblkZgiLCJzcGVsbCI6InRlc3QuYWRtaW4iLCJwaG9uZSI6IjEzMDAwMDAwMDAwIiwic3RhdHVzIjoiYWN0aXZlIiwibGV2ZWwiOjMsImxhc3RMb2dpbkF0IjoiMjAxOC0wNC0xN1QwOToyMjoxMS4wMzdaIiwicGFzc3dvcmQiOm51bGwsImF2YXRhclVybCI6bnVsbCwiY3JlYXRlZEF0IjoiMjAxNy0wNy0wN1QwMzowNjo1Ny4wMDBaIiwidXBkYXRlZEF0IjoiMjAxOC0wNC0xN1QwOToyMjoxMS4wMDBaIiwib3JnX2lkIjoyLCJzc29Ub2tlbiI6IjQzYzI4OTcwLWY5MmEtNDI0Zi1iMDkzLTNmNDNiNjQwNjg0ZiIsImlhdCI6MTUyMzk1NjkzMSwiZXhwIjoxNTI0NTYxNzMxfQ.zYg8o3Ow9HQNLQBgxlbKGpB5Rp4U8tcmUjjVI2W8l3o
     const timer = Observable.timer(0, 1000).subscribe(
       (it) => {
         if (it === 3) {
@@ -32,7 +41,7 @@ export default class Splash extends Component {
   }
 
   routerToLogin() {
-    this.props.navigation.navigate('Login')
+    this.props.navigation.dispatch(resetActionLogin)
   }
 
   render() {
@@ -41,12 +50,6 @@ export default class Splash extends Component {
         width: theme.screenWidth,
         height: theme.screenHeight
       }}>
-        {/* <TouchableOpacity activeOpaity={0.7}
-          style={{ width: 200, height: 200, backgroundColor: 'red'}}
-          onPress={() => {
-            this.props.navigation.navigate('Login')
-          }
-          } /> */}
         <Image style={{width: theme.screenWidth, height: theme.screenHeight}} source={SplashImage} />
       </View>
     );
